91bd3e128654a964b2079e261c2e77d7901521c5
Levente Mathe <levente.l.mathe@gmail.com>
2020-03-07 12:34:31 +0100

    add page and some global default styles

--- src/components/pages/ExerciseListPage.jsx
+++ src/components/pages/ExerciseListPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Page from './Page';
+
 const Styles = styled.div``;
 
-export default () => <Styles>List</Styles>;
+export default () => (
+  <Page>
+    <Styles>List</Styles>
+  </Page>
+);
